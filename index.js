2d3276c02f6ae4c9bd57a8f2d036283a6b53b421
sydney_p <sydneypapier23@gmail.com>
2024-08-02 19:01:33 +0200

    added display sum changes in html & js

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
-
-
 let messageEl = document.getElementById("message-el")
-console.log(messageEl)
+
+let sumEl = document.getElementById("sum-el")
 
 function startGame() {
+
+    sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
