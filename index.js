ea9b74ab304d63db47d235748cf858bf42832b96
sydney_p <sydneypapier23@gmail.com>
2024-08-02 18:35:33 +0200

    Created display message

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-// 1. Create two variables, firstCard and secondCard. 
-// Set their values to a random number between 2-11
-
-// 2. Create a variable, sum, and set it to the sum of the two cards
-
-let firstCard = 6
-let secondCard = 9
-
+let firstCard = 10
+let secondCard = 11
 let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
 
-console.log(sum)
+
+let messageEl = document.getElementById("message-el")
+console.log(messageEl)
+
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got BlackJack!"
+        hasBlackJack = true
+    } else { 
+        message ="You're out of the game!"
+        isAlive = false        
+    }
+
+    messageEl.textContext = message
+}
