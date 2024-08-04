e469848f57c9b4073cdb7964729ece82f6b78d75
sydney_p <sydneypapier23@gmail.com>
2024-08-04 22:44:23 +0200

    renamed the startGame function

--- index.js
+++ index.js
@@ -8,7 +8,15 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+/*this function makes sure the startGame btn from html works and calls
+on the renderGame function. */
 function startGame() {
+    renderGame() 
+}
+
+
+
+function renderGame() {
     cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
@@ -27,5 +35,5 @@ function newCard() {
     console.log("Drawing a new card from the deck!")
     let card = 6 /*created a card variable*/ 
     sum += card /*this adds the new card to the sum variable*/
-    startGame() /*call startGame()*/
+    renderGame() /*call renderGame()*/
 }
