3a8ca95d79cc817d9c3cfe0dfcc8c48827feea0d
sydney_p <sydneypapier23@gmail.com>
2024-08-05 00:43:37 +0200

    Used an array to solve card problem

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let firstCard = 10
 let secondCard = 11
+let cards = [firstCard, secondCard] //array - ordered list of items
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
@@ -14,10 +15,8 @@ function startGame() {
     renderGame() 
 }
 
-
-
 function renderGame() {
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
