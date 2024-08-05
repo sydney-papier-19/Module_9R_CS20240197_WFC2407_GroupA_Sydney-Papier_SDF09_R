6400dc9cc813ffa45b8a356557e049cb516f6a3e
sydney_p <sydneypapier23@gmail.com>
2024-08-05 02:45:25 +0200

    added js changes and html player change

--- index.js
+++ index.js
@@ -1,22 +1,50 @@
-let firstCard = 10
-let secondCard = 11
-let cards = [firstCard, secondCard] //array - ordered list of items
-let sum = firstCard + secondCard
+/*objects*/
+let player = {
+    name: "Per",
+    chips: 200
+}
+
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name + ": $" + player.chips
+
+/*The Math object */
+function getRandomCard() {
+    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
 
 /*this function makes sure the startGame btn from html works and calls
 on the renderGame function. */
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame() 
 }
 
 function renderGame() {
-    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    cardsEl.textContent = "Cards: " 
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -31,8 +59,10 @@ function renderGame() {
 }
 
 function newCard() {
-    console.log("Drawing a new card from the deck!")
-    let card = 6 /*created a card variable*/ 
+    if (isAlive === true && hasBlackJack === false) {
+    let card = getRandomCard()/*created a card variable*/ 
     sum += card /*this adds the new card to the sum variable*/
+    cards.push(card)/*push array*/
     renderGame() /*call renderGame()*/
+    }
 }
